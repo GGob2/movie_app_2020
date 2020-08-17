9bbef6baef1d4e1374adc35d25fd64af1c9219c2
GGOB2 <rkdaudwh13@naver.com>
2020-08-17 16:51:29 +0900

    0817/16:51

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
 
+function Food(props) {
+  console.log(props)
+return <h1>I like {props.fav}</h1>;
+}
 function App() {
-  
-  return  <div>Hello!!!</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi"/>
+    </div>
+  );
 }
 
 export default App;
