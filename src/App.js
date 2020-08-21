b27e3693936841b26e0009f5d644cd02c8806e85
GGOB2 <rkdaudwh13@naver.com>
2020-08-21 15:00:38 +0900

    '}' 를 뺴먹은 내 자신이 문제였던 것

--- src/App.js
+++ src/App.js
@@ -2,47 +2,50 @@ import React from "react";
 
 function Food({ name, picture }) {
   return (
-          <div>
-             <h2>I like {name} </h2>
-             <img src = {picture} />
-          </div>
-        );
-
+    <div>
+      <h2>I like {name} </h2>
+      <img src={picture} />
+    </div>
+  );
+}
 const foodILike = [
   {
     name: "Kimchi",
-    image: "",
+    image:
+      "https://bapuri.co.kr/new2/upload/menu_01/2016_12_16/hero_qGmO3_2016_12_16_15_13_27.jpg",
   },
 
   {
     name: "Samgyeopsal",
     image:
-      "",
+      "https://bapuri.co.kr/new2/upload/menu_01/2016_12_16/hero_qGmO3_2016_12_16_15_13_27.jpg",
   },
 
   {
     name: "Bibimbap",
-    image:"",
+    image:
+      "https://bapuri.co.kr/new2/upload/menu_01/2016_12_16/hero_qGmO3_2016_12_16_15_13_27.jpg",
   },
 
   {
     name: "Doncasu",
-    image: "",
+    image:
+      "https://bapuri.co.kr/new2/upload/menu_01/2016_12_16/hero_qGmO3_2016_12_16_15_13_27.jpg",
   },
 
   {
     name: "Kimbap",
-    image:"",
-      
+    image:
+      "https://bapuri.co.kr/new2/upload/menu_01/2016_12_16/hero_qGmO3_2016_12_16_15_13_27.jpg",
   },
 ];
 
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture = {dish.image}/>
-        ))}
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
