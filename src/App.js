01fdca3be314c206976e2ccf77bc6b49aea8e1df
GGOB2 <rkdaudwh13@naver.com>
2020-08-22 14:54:31 +0900

    study 'state'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
+
+class App extends React.Component {
+  // state는 반드시 클래스형 컴포넌트 안에서 소문자를 이용해서 사용해야한다.
+  // state는 객체형태의 데이터다.
+  state = {
+    count: 0,
+  };
+  // add 함수 정의
+
+  add = () => {
+    console.log('add');
+  }
+  // minus 함수 정의
+
+  minus = () => {
+    console.log('minus');
+  }
+
+  // class는 함수가 아니기 때문에, return문을 사용하려면 render() 함수를 사용해야 한다.
+  render() {
+    // class를 this로 받음 . <-- 연산자를 사용해서 객체 활용가능
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick ={this.add}>Add</button>
+        <button onClick ={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
+
+/* State를 배우기 위한 주석
 
 // 실질적으로 사용자에게 보여지는 부분
 // name, picture, rating을 App에서 받아옴
@@ -82,5 +114,6 @@ Food.propTypes = {
   // 가운데 자리에 위치한 number 자료형이여야 한다는 뜻
   rating: PropTypes.number,
 };
+*/
 
 export default App;
