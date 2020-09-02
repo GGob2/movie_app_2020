3d00adc4dca5e2ca36c343c9baebe0bc7c36a7a6
GGOB2 <rkdaudwh13@naver.com>
2020-09-03 01:07:14 +0900

    Updates

---
+++ precache-manifest.2edfbb59bde2dbbd8e4a9add9b494323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3e43f88a6831a7947a1e0de31241fd",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "46b8634767247008b62f",
+    "url": "/movie_app_2020/static/css/main.89587f7b.chunk.css"
+  },
+  {
+    "revision": "5d87d2a11609366ca700",
+    "url": "/movie_app_2020/static/js/2.2d8ea5b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.2d8ea5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b8634767247008b62f",
+    "url": "/movie_app_2020/static/js/main.38e8f278.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
